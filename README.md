5cadaac26845100f00d6a38ef31d56a447d59b4f
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-08-23 23:52:31 +1000

    created logic for rol

---
+++ README.md
@@ -0,0 +1,5 @@
+node documentation - https://nodejs.org/en
+
+node documentation - https://nodejs.org/docs/latest/api/
+
+npm Popular libraries & Packages - https://www.npmjs.com/
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const inquirer = require("inquirer");
+
+console.log("Dice roller app is now running!");
+
+/**
+ * Generate a random number between 1 and diceSize, inclusive.
+ * @param {number} diceSize The maximum that the dice can roll.
+ * @returns {number} The number rolled on the dice.
+ */
+function rollDice(diceSize = 20) {
+    let diceResult = 0;
+
+    // Dice roll logic here
+    diceResult = Math.ceil(Math.random() * diceSize);
+
+    return diceResult;
+}
+
+// console.log("Random dice roll result is:" + rollDice());
+
+const prompt = inquirer.createPromptModule();
+
+prompt([
+    {
+        type: "number",
+        name: "dice_size",
+        message: "What size of dice do you wanna rall?"
+    }
+]).then((answer) => {
+    // console.log("Raw answer data from the prompt package is:" + answer);
+    console.log("Raw answer data from the prompt package is:" + JSON.stringify(answer));
+    console.log(rollDice(answer.dice_size));
+});
